080b8ef55bb84a556d686ee84e024695efdb7d62
seanjahaupan <seanjahaupan@gmail.com>
2017-04-27 19:18:23 -0700

    basic structure

---
+++ navigation/Router.js
@@ -0,0 +1,15 @@
+import { createRouter } from '@expo/ex-navigation';
+
+import HomeScreen from '../screens/HomeScreen';
+import LinksScreen from '../screens/LinksScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+import PhotosScreen from '../screens/PhotosScreen';
+import RootNavigation from './RootNavigation';
+
+export default createRouter(() => ({
+  home: () => HomeScreen,
+  links: () => LinksScreen,
+  settings: () => SettingsScreen,
+  photos: () => PhotosScreen,
+  rootNavigation: () => RootNavigation,
+}));
