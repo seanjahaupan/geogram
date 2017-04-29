cc76ebf6a1f45b3d8c21a6506d12af25ab9e9a54
seanjahaupan <seanjahaupan@gmail.com>
2017-04-29 07:50:41 -0700

    completed adding likes functionality

--- screens/PhotosScreen.js
+++ screens/PhotosScreen.js
@@ -12,9 +12,15 @@ import {
 import { Card, Icon } from 'react-native-elements';
 import { MonoText } from '../components/StyledText';
 import DummyData from '../constants/DummyData';
+import Colors from '../constants/Colors';
 
 export default class PhotoScreen extends React.Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {likes:{1:0,2:0,3:2,4:0,5:0,6:0,7:0,8:0}, 
+                  liked:{1:false, 2:false, 3:false,4:false, 5:false,6:false, 7:false, 8:false}}
+  }
+
   static route = {
     navigationBar: {
       title: 'Photos'
@@ -33,13 +39,28 @@ export default class PhotoScreen extends React.Component {
           style={styles.likes}
         >
           <Icon
+            
+            color = {this.state.liked[item.id] ? 'red' : 'black'}
             name='heart'
-            type='simple-line-icon'
-            reverseColor = 'black'
+            type='entypo'
             containerStyle = {styles.iconStyle}
-            onPress = {() => console.log('You liked card '+ item.id)}
+            onPress = {() => {
+              const likes = {...this.state.likes};
+              const liked = {...this.state.liked};
+              //check if we liked the image already, if so, then decrease like count
+              if (liked[item.id]) {
+                likes[item.id]--;
+              } else {
+                likes[item.id]++;
+              }
+              //switch liked state
+              liked[item.id] = !liked[item.id]
+
+              this.setState({likes,liked})
+              //switch state
+              }}
           />
-          <Text>Number of Likes</Text>
+          <Text>  {this.state.likes[item.id]} Number of Likes</Text>
         </View>
       </Card>
     );
@@ -89,8 +110,7 @@ const styles = StyleSheet.create({
   },
 
   likes:{
-    flexDirection: 'row',
-    justifyContent: 'space-between'
+    flexDirection: 'row'
   },
   
 });
